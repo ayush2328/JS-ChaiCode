55f69e64c0ee641ffefc66940783af41fee8513b
Ayush Gupta <ayushofficial.2328@gmail.com>
2024-12-31 15:23:01 +0000

    Conversion Of dataTypes

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -1,1 +1,49 @@
-console.log("Ayush");
+
+// <-----------"Conversion to Number"------------>
+// let score = "Ayuashas";
+// console.log(typeof score);
+// console.log(typeof (score)); // both are true
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  // number
+// console.log(valueInNumber);  // number
+
+
+/*
+"23" (string)=> 23 (number)
+"23oct" (string) => NaN(number)
+"Ayush" (string) => NaN(number)
+null (object) => 0 (number)
+undefined (undefined) => Nan (number)
+true (boolean) => 1 (number)
+false (boolean) => 0 (number)
+*/
+
+
+// <-----------"Conversion to Boolean"------------>
+// let isLoggedIn = "124234535356464654";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*
+1 -> true; 0-> false
+"" -> false
+"Ayush" -> true
+null -> false
+undefined -> false
+*/
+
+// <-----------"Conversion to String"------------>
+let someNumber = undefined;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
+/*
+33 -> 33
+null -> null
+if u write any thing it convert in string it means in double or single quots
+*/
+
+// <-----------"Conversion to Object"------------>
